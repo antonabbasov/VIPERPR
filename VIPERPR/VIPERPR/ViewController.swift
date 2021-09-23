e8ca1034382756ca49884ab02f6118320bf0d754
Anton Abbasov <antonabbasov@gmail.com>
2021-09-23 20:59:12 +0300

    init

---
+++ VIPERPR/VIPERPR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPERPR
+//
+//  Created by Anton on 23.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
